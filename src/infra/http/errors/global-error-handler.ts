b775bc52aabe1984e36c3f481730fa592bb2e011
Luiz <l.eduardo8316@gmail.com>
2023-12-27 19:56:00 -0300

    feat: add new errors in global handler error

--- src/infra/http/errors/global-error-handler.ts
+++ src/infra/http/errors/global-error-handler.ts
@@ -1,5 +1,9 @@
 import { env } from '@/env'
 import { InvalidCredentialsError } from '@/errors/invalid-credentials-error'
+import { LateCheckInValidationError } from '@/errors/late-check-in-validation-error'
+import { MaxDistanceError } from '@/errors/max-distance-error'
+import { MaxNumbersOfCheckInError } from '@/errors/max-number-of-check-ins-error'
+import { ResourceNotFoundError } from '@/errors/resource-not-found'
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
 import { FastifyError, FastifyRequest, FastifyReply } from 'fastify'
 import { ZodError } from 'zod'
@@ -21,6 +25,22 @@ export async function globalErrorHandler(
 
   if (error instanceof InvalidCredentialsError) {
     return reply.status(401).send({ message: error.message })
+  }
+
+  if (error instanceof LateCheckInValidationError) {
+    return reply.status(422).send({ message: error.message })
+  }
+
+  if (error instanceof MaxDistanceError) {
+    return reply.status(422).send({ message: error.message })
+  }
+
+  if (error instanceof MaxNumbersOfCheckInError) {
+    return reply.status(422).send({ message: error.message })
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({ message: error.message })
   }
 
   if (env.NODE_ENV !== 'production') {
