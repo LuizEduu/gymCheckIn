291edf4d4c24dd302f08e4c829aec0f2708399ee
Luiz <l.eduardo8316@gmail.com>
2024-01-02 20:43:03 -0300

    feat: create auth middleware

--- src/infra/http/controllers/profile.ts
+++ src/infra/http/controllers/profile.ts
@@ -1,7 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/modules/user/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { undefined } from 'zod'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    ...user,
+    password_hash: undefined,
+  })
 }
