23bf648c1125b2785057beb3b8e421553a62df5c
Luiz <l.eduardo8316@gmail.com>
2024-01-02 19:52:40 -0300

    feat: add jwt config

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,15 @@ import fastify from 'fastify'
 
 import { userRoutes } from './infra/http/routes/user.routes'
 import { globalErrorHandler } from './infra/http/errors/global-error-handler'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
 
 export const app = fastify({
   logger: true,
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
 })
 
 app.register(userRoutes)
