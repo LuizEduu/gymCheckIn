1bd1cc88598b1a47ae8b8b50d0b41023f7774411
Luiz <l.eduardo8316@gmail.com>
2024-01-04 20:11:05 -0300

    feat: add supertest

--- src/infra/http/controllers/register.spec.ts
+++ src/infra/http/controllers/register.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {})
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  it('shoud be able to register', async () => {
+    beforeAll(async () => {
+      await app.ready()
+    })
+
+    afterAll(async () => {
+      await app.close()
+    })
+
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '1234567',
+    })
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password_hash: expect.any(String),
+        created_at: expect.any(Date),
+      }),
+    )
+  })
+})
