e6584c95f6517434a785d195bfac98d6d0c956f8
Luiz <l.eduardo8316@gmail.com>
2024-01-16 10:57:26 -0300

    feat: refresh token

---
+++ src/infra/http/controllers/users/refresh.ts
@@ -0,0 +1,50 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeAuthenticateUseCase } from '@/modules/user/use-cases/factories/make-authenticate-use-case'
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  const authenticateUseCase = makeAuthenticateUseCase()
+
+  const { user } = await authenticateUseCase.execute({
+    email,
+    password,
+  })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: user.id,
+      },
+    },
+  )
+
+  const refreshToken = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refreshToken', refreshToken, {
+      path: '/', // quais rotas da aplicação vão ter acesso
+      secure: true, // usando http define que o cookie vai ser encriptado
+      sameSite: true, // cookie só vai ser acessível dentro do mesmo dominio/site
+      httpOnly: true, // cookie só vai ser acessado apenas nos contextos de request e response do backend
+    })
+    .status(200)
+    .send({
+      token,
+    })
+}
--- src/infra/http/routes/users-routes.ts
+++ src/infra/http/routes/users-routes.ts
@@ -3,10 +3,12 @@ import { register } from '../controllers/users/register'
 import { authenticate } from '../controllers/users/authenticate'
 import { profile } from '../controllers/users/profile'
 import { verifyJWT } from '../middlewares/verify-jwt'
+import { refresh } from '../controllers/users/refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
